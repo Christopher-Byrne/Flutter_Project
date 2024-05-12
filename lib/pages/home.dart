09fa748bb0f5cb504fe6deec18ab61db511fffea
Christopher-Byrne <c.byrne199@gmail.com>
2024-05-12 18:17:42 +0100

    Added button to acess phones gallery

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,25 @@
+import 'dart:ffi';
+import 'dart:js';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
+   Future<Void> _getImageFromGallery(context) async {
+    final picker = ImagePicker();
+    final pickedImage = await picker.pickImage(source: ImageSource.gallery);
+
+    // Do something with the picked image, like display it in an Image widget
+    if (pickedImage != null) {
+      // You can use the pickedImage.path to display the image
+        // Navigate to the new page when the button is pressed
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => ImagePage()),
+            );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,6 +36,22 @@ class HomePage extends StatelessWidget {
           backgroundColor: Colors.black,
           centerTitle: true,
       ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: _getImageFromGallery(context),
+          child: Text("Select Image"),
+        ),
+      ),
     );
+  }
+
+}
+
+class ImagePage extends StatelessWidget {
+  const ImagePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }

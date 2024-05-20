7cccbedaf07020ea166a6f718ef2f3fd57ba7b29
Christopher-Byrne <c.byrne199@gmail.com>
2024-05-20 22:30:19 +0100

    Reads the hex representation of the image

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -11,7 +11,6 @@ class HomePage extends StatelessWidget {
 
     if (pickedImage != null) {
       Navigator.push(
-        // ignore: use_build_context_synchronously
         context,
         MaterialPageRoute(
           builder: (context) => ImagePage(imagePath: pickedImage.path),
@@ -45,10 +44,46 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class ImagePage extends StatelessWidget {
+class ImagePage extends StatefulWidget {
   final String imagePath;
 
   const ImagePage({super.key, required this.imagePath});
+
+  @override
+  _ImagePageState createState() => _ImagePageState();
+}
+
+class _ImagePageState extends State<ImagePage> {
+  late String hexCode = ''; // Initialize with an empty string
+
+  @override
+  void initState() {
+    super.initState();
+    // Read the image file and compute the hex code
+    _computeHexCode(widget.imagePath);
+  }
+
+  Future<void> _computeHexCode(String imagePath) async {
+    try {
+      // Read the image file as bytes
+      final bytes = await File(imagePath).readAsBytes();
+      // Convert bytes to hex string
+      hexCode = bytesToHex(bytes);
+      setState(() {}); // Update the UI
+    } catch (e) {
+      // Handle any errors that occur during file reading
+      hexCode = "Error reading file: $e";
+      setState(() {});
+    }
+  }
+
+  String bytesToHex(List<int> bytes) {
+    final buffer = StringBuffer();
+    for (var byte in bytes) {
+      buffer.write(byte.toRadixString(16).padLeft(2, '0'));
+    }
+    return buffer.toString();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -56,8 +91,27 @@ class ImagePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Selected Image'),
       ),
-      body: Center(
-        child: Image.file(File(imagePath)),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Image.file(File(widget.imagePath)),
+            const SizedBox(height: 20),
+            Expanded(
+              child: SingleChildScrollView(
+                child: TextField(
+                  readOnly: true,  // Make the TextField read-only
+                  maxLines: null,  // Allow for multiple lines
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Hex Code',
+                  ),
+                  controller: TextEditingController(text: hexCode),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }

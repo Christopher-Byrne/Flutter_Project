ab82af8499e29ed3fb28bc92c71d870c591917bc
Christopher-Byrne <c.byrne199@gmail.com>
2024-05-20 21:37:36 +0100

    Image Screen added

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,23 +1,22 @@
-import 'dart:ffi';
-import 'dart:js';
-
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-   Future<Void> _getImageFromGallery(context) async {
+
+  Future<void> _getImageFromGallery(BuildContext context) async {
     final picker = ImagePicker();
     final pickedImage = await picker.pickImage(source: ImageSource.gallery);
 
-    // Do something with the picked image, like display it in an Image widget
     if (pickedImage != null) {
-      // You can use the pickedImage.path to display the image
-        // Navigate to the new page when the button is pressed
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => ImagePage()),
-            );
+      Navigator.push(
+        // ignore: use_build_context_synchronously
+        context,
+        MaterialPageRoute(
+          builder: (context) => ImagePage(imagePath: pickedImage.path),
+        ),
+      );
     }
   }
 
@@ -25,33 +24,41 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
+        title: const Text(
           'My Test App',
           style: TextStyle(
             color: Colors.white,
             fontSize: 18,
-            fontWeight: FontWeight.bold
-            )
+            fontWeight: FontWeight.bold,
           ),
-          backgroundColor: Colors.black,
-          centerTitle: true,
+        ),
+        backgroundColor: Colors.black,
+        centerTitle: true,
       ),
       body: Center(
         child: ElevatedButton(
-          onPressed: _getImageFromGallery(context),
-          child: Text("Select Image"),
+          onPressed: () => _getImageFromGallery(context),
+          child: const Text("Select Image"),
         ),
       ),
     );
   }
-
 }
 
 class ImagePage extends StatelessWidget {
-  const ImagePage({super.key});
+  final String imagePath;
+
+  const ImagePage({super.key, required this.imagePath});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Selected Image'),
+      ),
+      body: Center(
+        child: Image.file(File(imagePath)),
+      ),
+    );
   }
 }
